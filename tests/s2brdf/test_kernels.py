5844041e76489536e9c2448ae4963c8799e848bf
hrodmn <henry.rodman@gmail.com>
2021-09-16 16:03:03 -0500

    update principal plane angles in unit tests

--- tests/s2brdf/test_kernels.py
+++ tests/s2brdf/test_kernels.py
@@ -26,8 +26,8 @@ def test_sec():
 
 def test_relative_azimuth():
     # saa == 120 degrees, vaa == 275 degrees
-    assert relative_azimuth(saa=2 * np.pi / 3, vaa=3 * np.pi / 2) == pytest.approx(np.pi / 6)
-    assert relative_azimuth(saa=np.pi / 2, vaa=3 * np.pi / 2) == 0.0
+    assert relative_azimuth(saa=2 * np.pi / 3, vaa=3 * np.pi / 2) == pytest.approx(5 * np.pi / 6)
+    assert relative_azimuth(saa=np.pi / 2, vaa=3 * np.pi / 2) == pytest.approx(np.pi)
 
 
 def test_calc_cos_xi():
@@ -36,9 +36,9 @@ def test_calc_cos_xi():
 
 
 def test_calc_kgeo():
-    # principal plain
+    # principal plane
     SZA = deg2rad(45)
-    SAA = deg2rad(180)
+    SAA = deg2rad(0)
     VAA = deg2rad(0)
     assert calc_kgeo(SZA, deg2rad(0), SAA, VAA) == pytest.approx(-1.1068191757647372)
     assert calc_kgeo(SZA, deg2rad(50), SAA, VAA) == pytest.approx(0.4675029273554563)
@@ -51,9 +51,9 @@ def test_calc_kgeo():
     assert calc_kgeo(SZA, deg2rad(50), SAA, VAA) == pytest.approx(2 * -0.6906509063504029, abs=0.04)
 
 def test_calc_kvol():
-    # principal plain
+    # principal plane
     SZA = deg2rad(45)
-    SAA = deg2rad(180)
+    SAA = deg2rad(0)
     VAA = deg2rad(0)
 
     # values estimated from Fig 2 in Lucht et al 2000
