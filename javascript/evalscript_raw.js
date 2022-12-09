46edf5c92550c901665298005a03d420660b8826
Jonas Viehweger <jonas.viehweger@sinergise.com>
2022-12-09 16:29:27 +0100

    Formatting

--- javascript/evalscript_raw.js
+++ javascript/evalscript_raw.js
@@ -3,20 +3,20 @@
 function setup() {
     return {
         input: ["B02", "B03", "B04", "B08", "B11", "B12", "sunAzimuthAngles", "sunZenithAngles", "viewAzimuthMean", "viewZenithMean"],
-        output: { 
+        output: {
             bands: 6,
             sampleType: "FLOAT32"
         },
         mosaicking: "TILE"
     };
 }
-  
+
 function evaluatePixel(sample) {
-    for(let i=0; i<sample.length; i++){
+    for (let i = 0; i < sample.length; i++) {
         // Only calculate the BRDF for pixels where the 
         // view geometry is available (on the edges of tiles
         // reflectance is available but not view geometry)
-        if(sample[i].viewAzimuthMean>1){
+        if (sample[i].viewAzimuthMean > 1) {
             var available = sample[i]
             var saa = deg2rad(available.sunAzimuthAngles);
             var sza = deg2rad(available.sunZenithAngles);
@@ -24,41 +24,41 @@ function evaluatePixel(sample) {
             var vza = deg2rad(available.viewZenithMean);
             var bands = Object.keys(f_values);
             return bands.map(band => calc_nbar(
-                available[band], 
-                f_values[band], 
+                available[band],
+                f_values[band],
                 sza, vza, saa, vaa));
         }
     }
-    return [0,0,0]
+    return [0, 0, 0]
 }
 
 // Kernel Parameters (Roy et al. 2017, Table 1)
 // [f_iso, f_geo, f_vol]
 var f_values = {
-    "B02": [0.0774,0.0079,0.0372], 
-    "B03": [0.1306,0.0178,0.0580], 
-    "B04": [0.1690,0.0227,0.0574], 
-    "B08": [0.3093,0.0330,0.1535], 
-    "B11": [0.3430,0.0453,0.1154], 
-    "B12": [0.2658,0.0387,0.0639]
+    "B02": [0.0774, 0.0079, 0.0372],
+    "B03": [0.1306, 0.0178, 0.0580],
+    "B04": [0.1690, 0.0227, 0.0574],
+    "B08": [0.3093, 0.0330, 0.1535],
+    "B11": [0.3430, 0.0453, 0.1154],
+    "B12": [0.2658, 0.0387, 0.0639]
 };
 
-function deg2rad(x){
+function deg2rad(x) {
     // Convert degrees to radians
     return x * Math.PI / 180;
 }
 
-function rad2deg(x){
+function rad2deg(x) {
     // Convert degrees to radians
     return x / Math.PI * 180;
 }
 
-function sec(x){
+function sec(x) {
     // Calculate the secant of a value
     return 1 / Math.cos(x);
 }
 
-function relative_azimuth(saa, vaa){
+function relative_azimuth(saa, vaa) {
     // Calculate relative azimuth angle
     // Angles in RAD !
     var phi = Math.abs(saa - vaa)
@@ -66,12 +66,12 @@ function relative_azimuth(saa, vaa){
     if (phi > Math.PI) {
         diff = 2 * Math.PI - phi;
     } else {
-       diff = phi;
+        diff = phi;
     }
     return diff;
 }
 
-function calc_kgeo(sza, vza, saa, vaa){
+function calc_kgeo(sza, vza, saa, vaa) {
     // Calculate the LiSparse kernel from Lucht et al. 2000
     // Angles in RAD !
 
@@ -81,7 +81,7 @@ function calc_kgeo(sza, vza, saa, vaa){
     // theta_prime = Math.atan(b / r * Math.tan(sza)) simplifies because b/r = 1
     var theta_prime = sza
     // vartheta_prime = Math.atan(b / r * Math.tan(vza)) simplifies because b/r = 1
-    var vartheta_prime =  vza
+    var vartheta_prime = vza
 
     //c 43 Lucht
     var cos_xi_prime = Math.cos(theta_prime) * Math.cos(vartheta_prime) + Math.sin(theta_prime) * Math.sin(vartheta_prime) * Math.cos(phi);
@@ -107,11 +107,11 @@ function calc_kgeo(sza, vza, saa, vaa){
     return kgeo;
 }
 
-function calc_cos_xi(theta, vartheta, phi){
+function calc_cos_xi(theta, vartheta, phi) {
     return Math.cos(theta) * Math.cos(vartheta) + Math.sin(theta) * Math.sin(vartheta) * Math.cos(phi);
 }
 
-function calc_kvol(sza, vza, saa, vaa){
+function calc_kvol(sza, vza, saa, vaa) {
     //Calculate the RossThick kernel (k_vol) from Lucht et al. 2000 equation 38
     // Angles in RAD !
     var phi = relative_azimuth(saa, vaa);
@@ -131,23 +131,23 @@ function calc_kvol(sza, vza, saa, vaa){
     return kvol;
 }
 
-function calc_rho_modis(sza, vza, saa, vaa, f){
+function calc_rho_modis(sza, vza, saa, vaa, f) {
     // Eq. 6 in Roy et al 2017, Eq. 37 in Lucht et al 2000
     var k_s = [1, calc_kgeo(sza, vza, saa, vaa), calc_kvol(sza, vza, saa, vaa)]
     var rho_modis = 0;
-    for(var i=0; i<k_s.length; i++) {
-        rho_modis += k_s[i]*f[i];
+    for (var i = 0; i < k_s.length; i++) {
+        rho_modis += k_s[i] * f[i];
     }
 
     return rho_modis;
 }
 
-function calc_c_lambda(sza, vza, saa, vaa, f){
+function calc_c_lambda(sza, vza, saa, vaa, f) {
     // Part 2 of Eq. 5 in Roy et al 2017
     return calc_rho_modis(sza, 0, saa, vaa, f) / calc_rho_modis(sza, vza, saa, vaa, f);
 }
 
-function calc_nbar(r_s2, f, sza, vza, saa, vaa){
+function calc_nbar(r_s2, f, sza, vza, saa, vaa) {
     //Part 1 of Eq. 5 in Roy et al 2017
     // r_s2: reflectance in band 
     // f: f values for band
